5f0535c084a5580576ed73916baf8c5e98a9c4f6
l3v3l <devl3v3l@gmail.com>
2015-11-05 14:23:35 +0000

    refractoring

--- js/index.js
+++ js/index.js
@@ -18,61 +18,54 @@ searchQuery.onkeypress = function (e) {
     if (e.keyCode == 13) {
         SearchWiki();
     }
-}
+};
 
 searchQuery.value = "portogalo";
 
 function SearchWiki() {
     list.innerHTML = "";
-    var s = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=20&prop=pageimages|extracts|info&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&piprop=original|thumbnail&inprop=url&gsrsearch=' + searchQuery.value + '&callback=?';
 
-    $.getJSON(s, function (data) {
+    q = '&generator=search' +
+        '&gsrnamespace=0' +
+        '&gsrlimit=20' +
+        '&gsrsearch=' + searchQuery.value;
 
-        var pages = data["query"]["pages"];
-        for (selector in pages) {
-            try {
-                var imageSrc = JSON.stringify(pages[selector]["thumbnail"]["original"]).replace(/\"/g, "");
-            } catch (err) {
-                var imageSrc = "images/placeholder.png";
-            }
-            try {
-                var thumbSrc = JSON.stringify(pages[selector]["thumbnail"]["source"]).replace(/\"/g, "");
-            } catch (err) {
-                var thumbSrc = "";
-            }
-            try {
-                var title = JSON.stringify(pages[selector]["title"]).replace(/\"/g, "");
-            } catch (err) {
-                var title = "";
-            }
-            try {
-                var extract = JSON.stringify(pages[selector]["extract"]).replace(/\"/g, "");
-            } catch (err) {
-                var extract = "";
-            }
-            try {
-                var fullUrl = JSON.stringify(pages[selector]["fullurl"]).replace(/\"/g, "");
-            } catch (err) {
-                var fullUrl = "";
-            }
-            try {
-                var pageId = JSON.stringify(pages[selector]["pageid"]).replace(/\"/g, "");
-            } catch (err) {
-                var pageId = "";
-            }
-            $(list).append(' <div class = "item" ><div class="coverImage"><a href=' + fullUrl + ' target="_blank"><img  src = ' + imageSrc + ' alt = "wiki"></a></div><div class="description"><a href=' + fullUrl + ' target="_blank"><h3> ' + title + ' </h3></a> <p> ' + extract + ' </p></div><div class="addButton"><input type="button" class="testbutton" value="save" onclick="saveToData(' + pageId + ',\'' + title + '\')"/></div></div >');
-        }
-    });
+    var arrayObj = getWikiPages(q);
+    for (selector in arrayObj) {
+        $(list).append(' <div class = "item" ><div class="coverImage"><a href=' + selector.fullUrl + ' target="_blank"><img  src = ' + selector.imageSrc + ' alt = "wiki"></a></div><div class="description"><a href=' + selector.fullUrl + ' target="_blank"><h3> ' + selector.title + ' </h3></a> <p> ' + selector.extract + ' </p></div><div class="addButton"><input type="button" class="testbutton" value="save" onclick="saveToData(' + selector.pageId + ',\'' + selector.title + '\')"/></div></div >');
+    }
+
 }
 
 function QueryWiki(idList) {
     savedList.innerHTML = "";
-    var b = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages|extracts|info&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&piprop=original|thumbnail&pageids=' + idList + '&callback=?';
 
-    $.getJSON(b, function (data) {
+    q = '&pageids=' + idList;
+    getWikiPages(q);
 
+    var arrayObj = getWikiPages(q);
+    for (selector in arrayObj) {
+        $(savedList).append(' <div class = "item" ><div class="coverImage"><a href=' + selector.fullUrl + ' target="_blank"><img  src = ' + selector.imageSrc + ' alt = "wiki"></a></div><div class="description"><a href=' + selector.fullUrl + ' target="_blank"><h3> ' + selector.title + ' </h3></a> <p> ' + selector.extract + ' </p></div><div class="addButton"><input type="button" class="testbutton" value="remove" onclick="removeFromData(' + selector.pageId + ')"/></div></div >');
+    }
 
+}
 
+function getWikiPages(q) {
+    q = 'https://en.wikipedia.org/w/api.php?format=json' +
+        '&action=query' +
+        '&prop=pageimages|extracts|info' +
+        '&inprop=url' +
+        '&callback=?' +
+        '&pilimit=max' +
+        '&exintro' +
+        '&explaintext' +
+        '&exsentences=1' +
+        '&exlimit=max' +
+        '&piprop=original|thumbnail' +
+        q;
+
+    var arrayObjects = [];
+    $.getJSON(q, function (data) {
         var pages = data["query"]["pages"];
         for (selector in pages) {
             try {
@@ -105,9 +98,20 @@ function QueryWiki(idList) {
             } catch (err) {
                 var pageId = "";
             }
-            $(savedList).append(' <div class = "item" ><div class="coverImage"><a href=' + fullUrl + ' target="_blank"><img  src = ' + imageSrc + ' alt = "wiki"></a></div><div class="description"><a href=' + fullUrl + ' target="_blank"><h3> ' + title + ' </h3></a> <p> ' + extract + ' </p></div><div class="addButton"><input type="button" class="testbutton" value="remove" onclick="removeFromData(' + pageId + ')"/></div></div >');
+
+            var item = {
+                imageSrc: imageSrc,
+                thumbSrc: thumbSrc,
+                title: title,
+                extract: extract,
+                fullUrl: fullUrl,
+                pageId: pageId
+            };
+            arrayObjects.push(item);
         }
     });
+
+    return arrayObjects;
 }
 
 function saveToData(pageid, title) {
