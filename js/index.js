608bd5a688a895cb5de3dcf342524493b67fdf66
l3v3l <devl3v3l@gmail.com>
2015-11-26 15:49:32 +0000

    added wikipedia url parsing in search (fixes #10), also order search results by levenshtein score

--- js/index.js
+++ js/index.js
@@ -30,6 +30,12 @@ searchQuery.value = "portogalo";
 
 function SearchWiki() {
     list.innerHTML = "";
+    
+    searchQuery.value = searchQuery.value.toLowerCase();
+    if(searchQuery.value.split(' ').length == 1){
+        searchQuery.value = searchQuery.value.replace(/.*wikipedia.org\/wiki\//,"");
+    }
+    
     q = '&generator=search' +
         '&gsrnamespace=0' +
         '&gsrlimit=20' +
@@ -103,21 +109,57 @@ function getWikiPages(q, callback) {
             } catch (err) {
                 var pageId = "";
             }
-
+            
             var item = {
                 imageSrc: imageSrc,
                 thumbSrc: thumbSrc,
                 title: title,
                 extract: extract,
                 fullUrl: fullUrl,
-                pageId: pageId
+                pageId: pageId,
+                rank:  s.levenshtein(searchQuery.value, title)
             };
             arrayObjects.push(item);
         }
+        arrayObjects = _.sortBy( arrayObjects, 'rank' );
         callback(arrayObjects);
     });
 
 }
+
+function disString(a, b){
+  if(a.length == 0) return b.length; 
+  if(b.length == 0) return a.length; 
+
+  var matrix = [];
+
+  // increment along the first column of each row
+  var i;
+  for(i = 0; i <= b.length; i++){
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  var j;
+  for(j = 0; j <= a.length; j++){
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for(i = 1; i <= b.length; i++){
+    for(j = 1; j <= a.length; j++){
+      if(b.charAt(i-1) == a.charAt(j-1)){
+        matrix[i][j] = matrix[i-1][j-1];
+      } else {
+        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                Math.min(matrix[i][j-1] + 1, // insertion
+                                         matrix[i-1][j] + 1)); // deletion
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+};
 
 function saveToData(pageid, title) {
     db.wikis.put({
